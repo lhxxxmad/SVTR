60192a8b074785436af1df39781382e8be44deca
chengxuxin <chengxuxin@bytedance.com>
2023-07-14 22:30:39 +0800

    sh

--- CiCo/CLCL/scripts/h2s.sh
+++ CiCo/CLCL/scripts/h2s.sh
@@ -2,6 +2,15 @@ export PYTHONWARNINGS='ignore:semaphore_tracker:UserWarning'
 split_hosts=$(echo $ARNOLD_WORKER_HOSTS | tr ":" "\n")
 split_hosts=($split_hosts)
 
+hdfs dfs -get hdfs://haruna/home/byte_arnold_lq_mlnlc/user/chengxuxin/lhx/VL/data/bsl5k.pth.tar
+mkdir /mnt/bd/cxx-third/SLRT/CiCo/I3D_feature_extractor/chpt
+mv bsl5k.pth.tar ../I3D_feature_extractor/chpt
+hdfs dfs -get hdfs://haruna/home/byte_arnold_lq_mlnlc/user/chengxuxin/lhx/VL/data/sign_feature.zip
+unzip sign_feature.zip -d ./
+
+hdfs dfs -get hdfs://haruna/home/byte_arnold_lq_mlnlc/user/chengxuxin/lhx/VL/EMCL-Net/tvr/models/ViT-B-32.pt
+mv ViT-B-32.pt ./modules
+
 # MSRVTT --do_train 1 \
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
 python3 -m torch.distributed.launch --nproc_per_node=8 \
