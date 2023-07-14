e0305da2430a672cae9923f8c4021d98832c1814
chengxuxin <chengxuxin@bytedance.com>
2023-07-14 22:14:55 +0800

    scripts

---
+++ CiCo/CLCL/scripts/h2s.sh
@@ -0,0 +1,11 @@
+export PYTHONWARNINGS='ignore:semaphore_tracker:UserWarning'
+split_hosts=$(echo $ARNOLD_WORKER_HOSTS | tr ":" "\n")
+split_hosts=($split_hosts)
+
+# MSRVTT --do_train 1 \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
+python3 -m torch.distributed.launch --nproc_per_node=8 \
+--master_addr ${ARNOLD_WORKER_0_HOST} \
+--master_port ${ARNOLD_WORKER_0_PORT} \
+main_task_retrieval.py \
+--do_train
